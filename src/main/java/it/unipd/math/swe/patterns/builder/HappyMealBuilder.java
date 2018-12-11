6b09274f91219ebac2feb9316e7691b006cd7feb
rcardin <riccardo.cardin@gmail.com>
2018-12-11 14:41:06 +0100

    Added adapter example

---
+++ src/main/java/it/unipd/math/swe/patterns/builder/HappyMealBuilder.java
@@ -0,0 +1,61 @@
+package it.unipd.math.swe.patterns.builder;
+
+import java.util.Objects;
+
+/**
+ * The Builder accumulates information to create a product. When the creation
+ * method is called, it can check whether all the precondition needed to build
+ * an Happy Meal are met.
+ *
+ * It exposes a fluent API.
+ *
+ * @see <a href="https://zeroturnaround.com/rebellabs/a-fluent-api-or-not-a-fluent-api-that-is-the-question/">
+ *     A Fluent API or not a fluent API? That is the Question!</a>
+ */
+public class HappyMealBuilder {
+    private String drink;
+    /**
+     * The fruit is not mandatory to build an Happy Meal (my gosh!)
+     */
+    private String fruit = null;
+    private String toy;
+    private String burger;
+    private String cake;
+
+    public HappyMealBuilder withDrink(String drink) {
+        this.drink = drink;
+        return this;
+    }
+
+    public HappyMealBuilder withFruit(String fruit) {
+        this.fruit = fruit;
+        return this;
+    }
+
+    public HappyMealBuilder withToy(String toy) {
+        this.toy = toy;
+        return this;
+    }
+
+    public HappyMealBuilder withBurger(String burger) {
+        this.burger = burger;
+        return this;
+    }
+
+    public HappyMealBuilder withCake(String cake) {
+        this.cake = cake;
+        return this;
+    }
+
+    public HappyMeal createHappyMeal() {
+        Objects.nonNull(drink);
+        Objects.nonNull(toy);
+        Objects.nonNull(burger);
+        Objects.nonNull(cake);
+        return new HappyMeal(drink,
+                             fruit,
+                             toy,
+                             burger,
+                             cake);
+    }
+}
